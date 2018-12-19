271dc84ce67f1509730b34d1bbb95c8c176abc44
Ananya Mukherjee <ananymuk@Ananyas-MacBook-Pro.local>
2018-12-18 23:56:50 -0800

    similarity calc code added

--- process.py
+++ process.py
@@ -7,6 +7,8 @@ import io
 import requests
 #from bs4 import BeautifulSoup
 import re
+import spacy
+from tqdm import tqdm
 
 def count_words(corpus, words):
     count = {}
@@ -30,11 +32,45 @@ def get_sentences(corpus, words):
     #pdb.set_trace()
     return sentence_dict
 
+def get_textbody_per_page(corpus):
+    text = []
+    for tags in corpus:
+        for idx in range(len(corpus[tags])):
+            text.append(corpus[tags][idx])
+    return text
+
+nlp = spacy.load("en_core_web_lg")
+def calc_sentence_similarity(textbody, textbody2 = None):#here textbody is a list of sentences
+    similarity_score = []
+    similarity_score2 = []
+    if textbody2 is None:
+        for idx1 in range(50):#len(textbody)):
+            for idx2 in range(1,50):#len(textbody)):
+                similarity_score.append(nlp(textbody[idx1]).similarity(nlp(textbody[idx2])))
+    else:
+        for idx1 in range(50):#len(textbody)):
+            for idx2 in range(50):#len(textbody2)):
+                similarity_score2.append(nlp(textbody[idx1]).similarity(nlp(textbody2[idx2])))
+    return similarity_score, similarity_score2
+
+
+#get all the text from electricity act
 page = requests.get("https://www.ontario.ca/laws/statute/98e15#BK1")
 to_be_removed = re.compile('<.*?>')
 all_data = datascraping(page)
 corpus = remove_html_tags(all_data, to_be_removed)
 words = ['comply', 'shall', 'must', 'oblige']
 print (count_words(corpus,words))
-#print (get_sentences(corpus, words))
-#pdb.set_trace()
+electricity_act_text = get_textbody_per_page(corpus)
+
+#get all the text from climate act
+page2 = requests.get("https://www.ontario.ca/laws/statute/16c07")
+all_data2 = datascraping(page2)
+corpus2 = remove_html_tags(all_data2, to_be_removed)
+print (count_words(corpus2, words))
+climate_act_text = get_textbody_per_page(corpus2)
+
+#calculate similarity between each sentence in electricity act
+for i in tqdm(range(10000)):
+    score1 = calc_sentence_similarity(electricity_act_text)
+    score2 = calc_sentence_similarity(electricity_act_text,climate_act_text )
