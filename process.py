76d19847a14c750f801a0d7f287ae28231efda33
Ananya Mukherjee <ananymuk@Ananyas-MacBook-Pro.local>
2018-12-19 21:48:44 -0800

    adding pickle files with similarity scores

--- process.py
+++ process.py
@@ -1,5 +1,5 @@
 from utils import datascraping, remove_html_tags
-import pickle as pkl, pdb
+import pickle as pkl, pdb, numpy as np
 import urllib.request, io, requests ,re
 import spacy
 from tqdm import tqdm
@@ -40,17 +40,20 @@ def get_textbody_per_page(corpus):
 nlp = spacy.load("en_core_web_lg")
 def calc_sentence_similarity(textbody, textbody2,no_of_sentences = None):#here textbody is a list of sentences
     similarity_score = {}
-    #similarity_score2 = []
     if no_of_sentences is None:
         for idx1 in tqdm(range(len(textbody))):#len(textbody)):
             for idx2 in range(len(textbody2)):#len(textbody)):
             #similarity_score.append(nlp(textbody[idx1]).similarity(nlp(textbody[idx2])))
                 similarity_score[(idx1, idx2)] = nlp(textbody[idx1]).similarity(nlp(textbody2[idx2]))
     else:
+        text1 = str(np.random.choice(textbody, no_of_sentences, replace = False))
+        text2 = str(np.random.choice(textbody2, no_of_sentences, replace = False))
+        #pdb.set_trace()
         for idx1 in tqdm(range(no_of_sentences)):#len(textbody)):
             for idx2 in range(no_of_sentences):#len(textbody)):
             #similarity_score.append(nlp(textbody[idx1]).similarity(nlp(textbody[idx2])))
-                similarity_score[(idx1, idx2)] = nlp(textbody[idx1]).similarity(nlp(textbody2[idx2]))
+                similarity_score[(idx1, idx2)] = nlp(text1[idx1]).similarity(nlp(text2[idx2]))
+    #pdb.set_trace()
     return similarity_score
 
 
@@ -58,26 +61,25 @@ def calc_sentence_similarity(textbody, textbody2,no_of_sentences = None):#here t
 def preprocessing(text):
     #tokenize the text
     tokenized_sents = [word_tokenize(i) for i in text] #this is a list of list
-    #remove punctuations
-    for idx in tokenized_sents:
+    #remove punctuations 
+    for idx in range(len(tokenized_sents)):
         #pdb.set_trace()
-        for i in idx:
-            i = re.sub(r'[^\w\s]','',i)
-    print (tokenized_sents[0])
-            #[i.translate(None, string.punctuation) for i in tokenized_sents[idx]]
-    '''
+        for i in range(len(tokenized_sents[idx])):
+            tokenized_sents[idx][i] = (re.sub(r'[^\w\s]','',tokenized_sents[idx][i])).lower()    
+
+    #remove stop words
     nltk_stopwords = nltk.corpus.stopwords.words('english')
-    print (nltk_stopwords)
-    words = []
-    for sentence in corpus:
-        for word in word_tokenize(sentence.lower):
-            if word not in nltk_stopwords:
-                words += [word]
-    '''
+    for idx in range(len(tokenized_sents)):
+        words = []
+        for w in tokenized_sents[idx]:
+            if w not in nltk_stopwords:
+                words.append(w)
+        tokenized_sents[idx] = words
 
-
-
-
+    for idx in range(len(tokenized_sents)):
+        tokenized_sents[idx] = " ".join(tokenized_sents[idx]).strip(" ")
+    return tokenized_sents #this is the processed list of text from an html page
+    
 #get all the text from electricity act
 page = requests.get("https://www.ontario.ca/laws/statute/98e15#BK1")
 to_be_removed = re.compile('<.*?>')
@@ -87,21 +89,25 @@ words = ['comply', 'shall', 'must', 'oblige']
 print (count_words(corpus,words))
 electricity_act_text = get_textbody_per_page(corpus)
 #pdb.set_trace()
-preprocessing(electricity_act_text)
-'''
+cleaned_electricity_act_text = preprocessing(electricity_act_text)
+
 #get all the text from climate act
 page2 = requests.get("https://www.ontario.ca/laws/statute/16c07")
 all_data2 = datascraping(page2)
 corpus2 = remove_html_tags(all_data2, to_be_removed)
 print (count_words(corpus2, words))
 climate_act_text = get_textbody_per_page(corpus2)
+cleaned_climate_act_text = preprocessing(climate_act_text)
+
 
 #calculate similarity between each sentence in electricity act
-score1 = calc_sentence_similarity(electricity_act_text,electricity_act_text)
-score2 = calc_sentence_similarity(electricity_act_text,climate_act_text )
+score1 = calc_sentence_similarity(cleaned_electricity_act_text,cleaned_electricity_act_text, 200)
+#print (score1)
+#pdb.set_trace()
+score2 = calc_sentence_similarity(electricity_act_text,climate_act_text,200 )
 #print (score1)
 #print (score2)
 pkl.dump( score1, open( "savesamescore.p", "wb" ) )
 pkl.dump(score2, open("savediffscore.p","wb"))
 pdb.set_trace()
-'''
+
