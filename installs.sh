bcc7b39ba46de43268c6a7b35aa9a5c1613c6053
Ananya Mukherjee <ananyamukh6@gmail.com>
2018-12-20 00:15:56 -0800

    Install spacy prereqs

--- installs.sh
+++ installs.sh
@@ -1,2 +1,3 @@
 pip install requests bs4 numpy spacy nltk matplotlib seaborn textstat
+python -m spacy download en
 pip install https://github.com/explosion/spacy-models/releases/download/en_core_web_lg-2.0.0/en_core_web_lg-2.0.0.tar.gz
--- readability.py
+++ readability.py
@@ -9,84 +9,82 @@ def break_sentences(text):
     return doc.sents 
   
 # Returns Number of Words in the text 
-def word_count(text): 
-    sentences = break_sentences(text) 
+def word_count(text):
+    sentences = break_sentences(text)
     words = 0
-    for sentence in sentences: 
-        words += len([token for token in sentence]) 
-    return words 
+    for sentence in sentences:
+        words += len([token for token in sentence])
+    return words
   
 # Returns the number of sentences in the text 
-def sentence_count(text): 
+def sentence_count(text):
     #pdb.set_trace()
     sentence = []
-    sentences = break_sentences(text) 
+    sentences = break_sentences(text)
     for i in sentences:
         sentence.append(i)
     #pdb.set_trace()
-    return len(sentence) 
+    return len(sentence)
   
-# Returns average sentence length 
-def avg_sentence_length(text): 
-    words = word_count(text) 
-    sentences = sentence_count(text) 
-    average_sentence_length = float(words / sentences) 
-    return average_sentence_length 
+# Returns average sentence length
+def avg_sentence_length(text):
+    words = word_count(text)
+    sentences = sentence_count(text)
+    average_sentence_length = float(words / sentences)
+    return average_sentence_length
   
-# Textstat is a python package, to calculate statistics from  
-# text to determine readability,  
-# complexity and grade level of a particular corpus. 
-# Package can be found at https://pypi.python.org/pypi/textstat 
-def syllables_count(word): 
-    return textstatistics().syllable_count(word) 
+# Textstat is a python package, to calculate statistics from
+# text to determine readability,
+# complexity and grade level of a particular corpus.
+# Package can be found at https://pypi.python.org/pypi/textstat
+def syllables_count(word):
+    return textstatistics().syllable_count(word)
   
-# Returns the average number of syllables per 
-# word in the text 
-def avg_syllables_per_word(text): 
-    syllable = syllables_count(text) 
-    words = word_count(text) 
-    ASPW = float(syllable) / float(words) 
-    return legacy_round(ASPW, 1) 
+# Returns the average number of syllables per
+# word in the text
+def avg_syllables_per_word(text):
+    syllable = syllables_count(text)
+    words = word_count(text)
+    ASPW = float(syllable) / float(words)
+    return legacy_round(ASPW, 1)
   
-# Return total Difficult Words in a text 
-def difficult_words(text): 
+# Return total Difficult Words in a text
+def difficult_words(text):
+
+    # Find all words in the text
+    words = []
+    sentences = break_sentences(text)
+    for sentence in sentences:
+        words += [str(token) for token in sentence]
+
+    # difficult words are those with syllables >= 2
+    # easy_word_set is provide by Textstat as
+    # a list of common words
+    diff_words_set = set()
+    for word in words:
+        syllable_count = syllables_count(word)
+        if word not in easy_word_set and syllable_count >= 2:
+            diff_words_set.add(word)
+
+    return len(diff_words_set)
   
-    # Find all words in the text 
-    words = [] 
-    sentences = break_sentences(text) 
-    for sentence in sentences: 
-        words += [str(token) for token in sentence] 
-  
-    # difficult words are those with syllables >= 2 
-    # easy_word_set is provide by Textstat as  
-    # a list of common words 
-    diff_words_set = set() 
-      
-    for word in words: 
-        syllable_count = syllables_count(word) 
-        if word not in easy_word_set and syllable_count >= 2: 
-            diff_words_set.add(word) 
-  
-    return len(diff_words_set) 
-  
-# A word is polysyllablic if it has more than 3 syllables 
-# this functions returns the number of all such words  
-# present in the text 
-def poly_syllable_count(text): 
+# A word is polysyllablic if it has more than 3 syllables
+# this functions returns the number of all such words
+# present in the text
+def poly_syllable_count(text):
     count = 0
-    words = [] 
-    sentences = break_sentences(text) 
-    for sentence in sentences: 
-        words += [token for token in sentence] 
-      
-  
-    for word in words: 
-        syllable_count = syllables_count(word) 
-        if syllable_count >= 3: 
+    words = []
+    sentences = break_sentences(text)
+    for sentence in sentences:
+        words += [token for token in sentence]
+
+    for word in words:
+        syllable_count = syllables_count(word)
+        if syllable_count >= 3:
             count += 1
-    return count 
-  
-def flesch_reading_ease(text): 
+    return count
+
+def flesch_reading_ease(text):
     """ 
         Implements Flesch Formula: 
         Reading Ease score = 206.835 - (1.015 × ASL) - (84.6 × ASW) 
